11d18a6b979f48d2ef148e6f2b0b18c995d75f75
Bijay Kumar Yadav <bijayy33@gmail.com>
2018-09-30 03:42:18 +0545

    Adding the button for add, list, calculate and dlete expenses CActivities commit

---
+++ app/src/main/java/com/example/bijay/expensemanagement/CalculateExpensesActivity.java
@@ -0,0 +1,13 @@
+package com.example.bijay.expensemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculateExpensesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_expenses);
+    }
+}
---
+++ app/src/main/java/com/example/bijay/expensemanagement/PrepareListActivity.java
@@ -0,0 +1,13 @@
+package com.example.bijay.expensemanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrepareListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prepare_list);
+    }
+}
